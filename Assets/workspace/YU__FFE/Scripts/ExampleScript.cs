205fdff4ed7c4c51a5fd79e7ff208c622e6278d1
YU__FFE <yu0.0ffe@gmail.com>
2025-03-06 16:29:28 +0900

    예제 케이스 및 workspace
    <<다음부터는 branch 생성 예정>>

---
+++ Assets/workspace/YU__FFE/Scripts/ExampleScript.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace workspace.YU__FFE.Scripts {
+    public class ExampleScript : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
+
