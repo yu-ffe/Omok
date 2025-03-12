d6551bcf2eafdb1466a388d4ba39ed818bb56051
YU__FFE <yu0.0ffe@gmail.com>
2025-03-12 17:15:32 +0900

    .
    .

--- Assets/workspace/YU__FFE/Scripts/Notation/Notation.cs
+++ Assets/workspace/YU__FFE/Scripts/Notation/Notation.cs
@@ -1,6 +1,9 @@
 
+
+using System.Collections.Generic;
+
 namespace workspace.YU__FFE.Scripts.Notation {
     public class Notation {
-        
+        private List<MatchRecord> _notationList = new List<MatchRecord>();
     }
 }
