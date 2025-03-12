edfb0f4adbd88463d66693a7acbc7222671c720e
KimHyeun <tenshi1207@gmail.com>
2025-03-12 16:33:31 +0900

    fix 모든 스크립트 네임스페이스 추가

--- Assets/workspace/KimHyeun/Scripts/RecordManager.cs
+++ Assets/workspace/KimHyeun/Scripts/RecordManager.cs
@@ -2,132 +2,133 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RecordManager : MonoBehaviour
-{
-    [Header("기보 스크롤 뷰 필수 할당")]
-    [SerializeField] ScrollViewSet scrollViewSet;
-
-    List<Sprite> resultSpriteList;
-    List<string> recordNameList;
-    List<string> nickNameList;
-    List<int> dateList;
-
-    private void Start()
+namespace KimHyeun {
+    public class RecordManager : MonoBehaviour
     {
-        resultSpriteList = new List<Sprite>();
-        recordNameList = new List<string>();
-        nickNameList = new List<string>();
-        dateList = new List<int>();
+        [Header("기보 스크롤 뷰 필수 할당")]
+        [SerializeField] ScrollViewSet scrollViewSet;
 
-        GetRecordData();
-    }
+        List<Sprite> resultSpriteList;
+        List<string> recordNameList;
+        List<string> nickNameList;
+        List<int> dateList;
 
-    public void GetRecordData() // 기보 팝업 오픈 시 호출
-    {
-        ResetData(); // 초기화
-
-
-
-        // 기보 데이터 불러오기 필요  (playerpref 기록 불러오기)
-
-
-
-
-       // scrollViewSet.StageSelectPopSet(GetMaxCellNum());
-    }
-
-
-    // TODO 기보 플레이 호출 기능 (기보 시스템 구축 대기)
-
-    // TODO 기보 제거 기능
-    public void RemoveRecord(int index)
-    {
-        Debug.Log($"{index}인덱스 기보 제거(RecordManager)");
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    void ResetData()
-    {
-        resultSpriteList.Clear();
-        recordNameList.Clear();
-        nickNameList.Clear();
-        dateList.Clear();
-    }
-
-    public int GetMaxCellNum()
-    {
-        return resultSpriteList.Count;
-    }
-
-
-    public Sprite GetSprite(int index)
-    {
-        if (resultSpriteList.Count > index)
+        private void Start()
         {
-            return resultSpriteList[index];
+            resultSpriteList = new List<Sprite>();
+            recordNameList = new List<string>();
+            nickNameList = new List<string>();
+            dateList = new List<int>();
+
+            GetRecordData();
         }
 
-        else
+        public void GetRecordData() // 기보 팝업 오픈 시 호출
         {
-            return null;
-        }
-    }
+            ResetData(); // 초기화
 
-    public string GetRecordName(int index)
-    {
-        if (recordNameList.Count > index)
-        {
-            return recordNameList[index];
-        }
 
-        else
-        {
-            return null;
-        }
-    }
 
-    public string GetName(int index)
-    {
-        if (nickNameList.Count > index)
-        {
-            return nickNameList[index];
+            // 기보 데이터 불러오기 필요  (playerpref 기록 불러오기)
+
+
+
+
+            // scrollViewSet.StageSelectPopSet(GetMaxCellNum());
         }
 
-        else
-        {
-            return null;
-        }
-    }
 
-    // TODO 날짜 형식 변환 필요
-    public int GetDate(int index)
-    {
-        if (dateList.Count > index)
+        // TODO 기보 플레이 호출 기능 (기보 시스템 구축 대기)
+
+        // TODO 기보 제거 기능
+        public void RemoveRecord(int index)
         {
-            return dateList[index];
+            Debug.Log($"{index}인덱스 기보 제거(RecordManager)");
         }
 
-        else
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        void ResetData()
         {
-            return 0;
+            resultSpriteList.Clear();
+            recordNameList.Clear();
+            nickNameList.Clear();
+            dateList.Clear();
         }
-    }
+
+        public int GetMaxCellNum()
+        {
+            return resultSpriteList.Count;
+        }
+
+
+        public Sprite GetSprite(int index)
+        {
+            if (resultSpriteList.Count > index)
+            {
+                return resultSpriteList[index];
+            }
+
+            else
+            {
+                return null;
+            }
+        }
+
+        public string GetRecordName(int index)
+        {
+            if (recordNameList.Count > index)
+            {
+                return recordNameList[index];
+            }
+
+            else
+            {
+                return null;
+            }
+        }
+
+        public string GetName(int index)
+        {
+            if (nickNameList.Count > index)
+            {
+                return nickNameList[index];
+            }
+
+            else
+            {
+                return null;
+            }
+        }
+
+        // TODO 날짜 형식 변환 필요
+        public int GetDate(int index)
+        {
+            if (dateList.Count > index)
+            {
+                return dateList[index];
+            }
+
+            else
+            {
+                return 0;
+            }
+        }
 
 
 
@@ -138,36 +139,38 @@ public class RecordManager : MonoBehaviour
 
 
 
-    private static RecordManager _instance;
+        private static RecordManager _instance;
 
-    public static RecordManager Instance
-    {
-        get
+        public static RecordManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = FindObjectOfType<RecordManager>();
+                    if (_instance == null)
+                    {
+                        GameObject obj = new GameObject();
+                        obj.name = typeof(RecordManager).Name;
+                        _instance = obj.AddComponent<RecordManager>();
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        private void Awake()
         {
             if (_instance == null)
             {
-                _instance = FindObjectOfType<RecordManager>();
-                if (_instance == null)
-                {
-                    GameObject obj = new GameObject();
-                    obj.name = typeof(RecordManager).Name;
-                    _instance = obj.AddComponent<RecordManager>();
-                }
+                _instance = this as RecordManager;
+                DontDestroyOnLoad(gameObject);
             }
-            return _instance;
-        }
-    }
-
-    private void Awake()
-    {
-        if (_instance == null)
-        {
-            _instance = this as RecordManager;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
+            else
+            {
+                Destroy(gameObject);
+            }
         }
     }
 }
+
--- Assets/workspace/KimHyeun/Scripts/ShopManager.cs
+++ Assets/workspace/KimHyeun/Scripts/ShopManager.cs
@@ -2,113 +2,114 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShopManager : MonoBehaviour
-{
-    [Header("상점 스크롤 뷰 필수 할당")]
-    [SerializeField] ScrollViewSet scrollViewSet;
-
-    [Header("필수 할당")]
-    [SerializeField] Sprite[] itemSprites;
-    [SerializeField] string[] itemNames;
-    [SerializeField] int[] nums;
-    [SerializeField] int[] prices;
-
-
-    private void Start()
+namespace KimHyeun {
+    public class ShopManager : MonoBehaviour
     {
-        /// 테스트
-        GetItemData();
-    }
+        [Header("상점 스크롤 뷰 필수 할당")]
+        [SerializeField] ScrollViewSet scrollViewSet;
+
+        [Header("필수 할당")]
+        [SerializeField] Sprite[] itemSprites;
+        [SerializeField] string[] itemNames;
+        [SerializeField] int[] nums;
+        [SerializeField] int[] prices;
 
 
-    public void GetItemData() // 아이템 팝업 오픈 시 호출
-    {
-        scrollViewSet.StageSelectPopSet(GetMaxCellNum());
-    }
-
-
-    
-    public void BuyCoin(int index) // 셀 클릭 시 코인 획득
-    {
-        SessionManager.UserSession userSession = SessionManager.GetSession(SessionManager.currentUserId);
-
-        userSession.Coins = userSession.Coins + nums[index];
-
-        SessionManager.UpdateSession(SessionManager.currentUserId, userSession.Coins, userSession.Grade, userSession.RankPoint);
-
-
-        /* // 테스트 코드
-        SessionManager.UserSession userSession = SessionManager.GetSession(SessionManager.GetAllUserIds()[0]);    
-        userSession.Coins = userSession.Coins + nums[index];
-        SessionManager.UpdateSession(SessionManager.GetAllUserIds()[0], userSession.Coins, userSession.Grade, userSession.RankPoint);
-        */
-    }
-
-
-
-
-
-
-
-
-    public int GetMaxCellNum()
-    {
-        return itemNames.Length;
-    }
-
-
-
-    public Sprite GetSprite(int index)
-    {
-        if (itemSprites.Length > index)
+        private void Start()
         {
-            return itemSprites[index];
+            /// 테스트
+            GetItemData();
         }
 
-        else
-        {
-            return null;
-        }
-    }
 
-    public string GetName(int index)
-    {
-        if (itemNames.Length > index)
+        public void GetItemData() // 아이템 팝업 오픈 시 호출
         {
-            return itemNames[index];
+            scrollViewSet.StageSelectPopSet(GetMaxCellNum());
         }
 
-        else
-        {
-            return null;
-        }
-    }
 
-    public int GetNum(int index)
-    {
-        if (nums.Length > index)
-        {
-            return nums[index];
-        }
 
-        else
+        public void BuyCoin(int index) // 셀 클릭 시 코인 획득
         {
-            return 0;
-        }
-    }
+            SessionManager.UserSession userSession = SessionManager.GetSession(SessionManager.currentUserId);
 
-    public int GetPrice(int index)
-    {
-        if (prices.Length > index)
-        {
-            return prices[index];
+            userSession.Coins = userSession.Coins + nums[index];
+
+            SessionManager.UpdateSession(SessionManager.currentUserId, userSession.Coins, userSession.Grade, userSession.RankPoint);
+
+
+            /* // 테스트 코드
+            SessionManager.UserSession userSession = SessionManager.GetSession(SessionManager.GetAllUserIds()[0]);    
+            userSession.Coins = userSession.Coins + nums[index];
+            SessionManager.UpdateSession(SessionManager.GetAllUserIds()[0], userSession.Coins, userSession.Grade, userSession.RankPoint);
+            */
         }
 
-        else
+
+
+
+
+
+
+
+        public int GetMaxCellNum()
         {
-            return 0;
+            return itemNames.Length;
         }
-    }
+
+
+
+        public Sprite GetSprite(int index)
+        {
+            if (itemSprites.Length > index)
+            {
+                return itemSprites[index];
+            }
+
+            else
+            {
+                return null;
+            }
+        }
+
+        public string GetName(int index)
+        {
+            if (itemNames.Length > index)
+            {
+                return itemNames[index];
+            }
+
+            else
+            {
+                return null;
+            }
+        }
+
+        public int GetNum(int index)
+        {
+            if (nums.Length > index)
+            {
+                return nums[index];
+            }
+
+            else
+            {
+                return 0;
+            }
+        }
+
+        public int GetPrice(int index)
+        {
+            if (prices.Length > index)
+            {
+                return prices[index];
+            }
+
+            else
+            {
+                return 0;
+            }
+        }
 
 
 
@@ -121,36 +122,38 @@ public class ShopManager : MonoBehaviour
 
 
 
-    private static ShopManager _instance;
+        private static ShopManager _instance;
 
-    public static ShopManager Instance
-    {
-        get
+        public static ShopManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = FindObjectOfType<ShopManager>();
+                    if (_instance == null)
+                    {
+                        GameObject obj = new GameObject();
+                        obj.name = typeof(ShopManager).Name;
+                        _instance = obj.AddComponent<ShopManager>();
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        private void Awake()
         {
             if (_instance == null)
             {
-                _instance = FindObjectOfType<ShopManager>();
-                if (_instance == null)
-                {
-                    GameObject obj = new GameObject();
-                    obj.name = typeof(ShopManager).Name;
-                    _instance = obj.AddComponent<ShopManager>();
-                }
+                _instance = this as ShopManager;
+                DontDestroyOnLoad(gameObject);
             }
-            return _instance;
-        }
-    }
-
-    private void Awake()
-    {
-        if (_instance == null)
-        {
-            _instance = this as ShopManager;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
+            else
+            {
+                Destroy(gameObject);
+            }
         }
     }
 }
+
