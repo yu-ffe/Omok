49115066dbe525a8b04f023401c05c92e8181482
ham6 <hammin96@gmail.com>
2025-03-13 09:54:47 +0900

    test
    test

--- Assets/workspace/Ham6/03 Sctipts/Game/GameLogic.cs
+++ Assets/workspace/Ham6/03 Sctipts/Game/GameLogic.cs
@@ -7,7 +7,7 @@ public class GameLogic : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //..
     }
 
     // Update is called once per frame
