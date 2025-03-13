6073c156e9daa904e2573d3e4703d92d29753615
MaribinStudio <40285905+WooNaBin@users.noreply.github.com>
2025-03-13 16:03:20 +0900

    [fix] Shop,Record,Ranking 패널 변경 스크립트 수정 ->Show()에서 도현님의 매니저에 연결. 호출 추가

--- Assets/workspace/WB/Scripts/RankingPanel.cs
+++ Assets/workspace/WB/Scripts/RankingPanel.cs
@@ -3,7 +3,27 @@ using System.Collections.Generic;
 using KimHyeun;
 using UnityEngine;
 
-public class RankingPanel : MonoBehaviour
+public class RankingPanel : WB.UI_Panel
 {
     public ScrollViewSet scrollView;
+
+
+    public override void Show()
+    {
+        // 매니저 스크롤 뷰에 해당 스크롤 뷰 할당
+        // RankingManager.Instance.SetScrollView(scrollView);
+        RankingManager.Instance.GetUserData();
+    }
+    public override void Hide()
+    {
+    }
+
+    public override void OnDisable()
+    {
+    }
+
+    public override void OnEnable()
+    {
+    }
+
 }
--- Assets/workspace/WB/Scripts/ShopPanel.cs
+++ Assets/workspace/WB/Scripts/ShopPanel.cs
@@ -3,7 +3,28 @@ using System.Collections.Generic;
 using KimHyeun;
 using UnityEngine;
 
-public class ShopPanel : MonoBehaviour
+public class ShopPanel : WB.UI_Panel
 {
     public ScrollViewSet scrollView;
+
+
+    public override void Show()
+    {
+        // 매니저 스크롤 뷰에 해당 스크롤 뷰 할당
+        // ShopManager.Instance.SetScrollView(scrollView);
+        ShopManager.Instance.GetItemData();
+    }
+
+    public override void Hide()
+    {
+    }
+
+    public override void OnDisable()
+    {
+    }
+
+    public override void OnEnable()
+    {
+    }
+
 }
