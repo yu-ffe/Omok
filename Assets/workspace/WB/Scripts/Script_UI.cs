71bf4489c9a86be0f8e5ed78c880529b98adf484
MaribinStudio <40285905+WooNaBin@users.noreply.github.com>
2025-03-13 11:37:56 +0900

    [add] 게임 패널 추가 스크립트,기본 게임패널 프리팹 전체 스크립트 폴더 이동,

--- Assets/workspace/WB/Scripts/Script_UI.cs
+++ Assets/workspace/WB/Scripts/Script_UI.cs
@@ -4,13 +4,6 @@ using UnityEngine.Events;
 
 namespace WB
 {
-    public interface IPopopUI
-    {
-        public void Show(string msg, string okText = null, string cancelText = null, UnityAction okAction = null, UnityAction cancelAction = null);
-        public void Hide();
-        // public void Refresh();
-    }
-
     public abstract class Singleton<T> where T : Singleton<T>
     {
         private static T instance;
