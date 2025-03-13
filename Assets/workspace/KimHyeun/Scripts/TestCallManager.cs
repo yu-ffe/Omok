87080ad0b4855ecfd2a703345bacab577dc7db12
KimHyeun <tenshi1207@gmail.com>
2025-03-13 15:50:17 +0900

    refactor 메인 프로필 버튼 클릭 이벤트 베이스

--- Assets/workspace/KimHyeun/Scripts/TestCallManager.cs
+++ Assets/workspace/KimHyeun/Scripts/TestCallManager.cs
@@ -18,10 +18,10 @@ namespace KimHyeun {
             SessionManager.currentUserId = SessionManager.GetAllUserIds()[0]; // 임의의 1번 유저를 로그인한 유저로 설정
 
 
-            // TODO 랭킹 팝업 호출 시 실행 필요
+            // 랭킹 팝업 호출 시 실행
             RankingManager.Instance.GetUserData(); 
 
-            // TODO 상점 팝업 호출 시 실행 필요
+            // 상점 팝업 호출 시 실행
             ShopManager.Instance.GetItemData(); 
 
             // TODO 로그인 시 호출, 상점 코인 구매 시 호출, 게임 종료 후 프로필 정보로 돌아가면 호출 -> 프로필 정보 변경 시 호출
