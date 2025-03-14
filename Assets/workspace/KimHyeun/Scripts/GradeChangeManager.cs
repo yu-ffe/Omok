704d122fc4012ea6bb6da279caa8dae7cd287249
KimHyeun <tenshi1207@gmail.com>
2025-03-14 11:49:08 +0900

    test 게임 결과에 따른 승급 테스트

--- Assets/workspace/KimHyeun/Scripts/GradeChangeManager.cs
+++ Assets/workspace/KimHyeun/Scripts/GradeChangeManager.cs
@@ -5,12 +5,6 @@ using UnityEngine;
 namespace KimHyeun {
     public class GradeChangeManager
     {
-        /*
-         승패 반환 함수? - 게임 종료 시?
-    - 승급/강등 애니메이션? -> 형태만 구현? 게임종료 처리쪽?
-         */
-
-
         public static void GradeUpdate(string userId, UserSession userSession, GameResult gameResultType) // 승패 결과를 받아서 유저 세션에 저장 (변경된 급수는 유저 세션에서 접근)
         {
             if (userSession != null)
@@ -18,8 +12,7 @@ namespace KimHyeun {
                 switch (gameResultType)
                 {
                     case GameResult.Win:
-                        Debug.Log($"{userSession.Nickname} 플레이어 승리에 따른 승급 계산 실행");
-
+                        
                         if(userSession.Grade > 1) // 1급 보다 낮은 급수일때만 승급 포인트 조절
                         {
                             userSession.RankPoint = userSession.RankPoint + 1;
@@ -37,8 +30,7 @@ namespace KimHyeun {
                         break;
 
                     case GameResult.Lose:
-                        Debug.Log($"{userSession.Nickname} 플레이어 패배에 따른 승급 계산 실행");
-
+                        
                         if (userSession.Grade < 18) // 18급 보다 높은 급수일때만 승급 포인트 조절
                         {
                             userSession.RankPoint = userSession.RankPoint - 1;
