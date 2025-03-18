a8d4a9e53a00a31a77164e6af6ada3db9ef37d4f
KimHyeun <tenshi1207@gmail.com>
2025-03-18 15:57:18 +0900

    feat ai 알고리즘 작성 준비

---
+++ Assets/workspace/KimHyeun/Scripts/OmokAI.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using workspace.Ham6._03_Sctipts.Game;
+
+public class OmokAI : MonoBehaviour
+{
+    Constants.PlayerType[,] board;
+
+    public OmokAI(Constants.PlayerType[,] board)
+    {
+        this.board = board;
+    }
+
+
+
+
+}
