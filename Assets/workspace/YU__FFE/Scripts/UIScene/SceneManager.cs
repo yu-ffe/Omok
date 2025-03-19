9aa7f60239d6108970553046555b612638f3d052
YU__FFE <yu0.0ffe@gmail.com>
2025-03-19 09:59:18 +0900

    Reapply "feat: 서버 연동 시스템 완료, data연결 시작"
    This reverts commit 003fb6f356f678bdef16c8a021c7ace6941104d4.

--- Assets/workspace/YU__FFE/Scripts/UIScene/SceneManager.cs
+++ Assets/workspace/YU__FFE/Scripts/UIScene/SceneManager.cs
@@ -1,37 +1,18 @@
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-
-namespace workspace.YU__FFE.Scripts.UIScene {
+namespace workspace.YU__FFE.Scripts {
     public class SceneManager : Singleton<SceneManager> {
+        private SceneManager() { }
 
-        private SceneManager() {
-        }
-
-        // 씬 인덱스를 사용하여 씬을 로드
-        public void LoadScene(int sceneNum) {
-            UnityEngine.SceneManagement.SceneManager.LoadScene(sceneNum);
-        }
-
-        // 씬 이름을 사용하여 씬을 로드
         public void LoadScene(string sceneName) {
-            Debug.Log($"[SceneManager] LoadScene: {sceneName}");
             UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
         }
 
-        // 현재 씬을 다시 로드
+        public void LoadSceneAsync(string sceneName) {
+            UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(sceneName);
+        }
+
         public void ReloadCurrentScene() {
             string currentScene = UnityEngine.SceneManagement.SceneManager.GetActiveScene().name;
             UnityEngine.SceneManagement.SceneManager.LoadScene(currentScene);
         }
-
-        bool n = true;
-        public void repeatScene() {
-            string scene1 = "DataTestScene";
-            string scene2 = "ServerTestScene";
-            UnityEngine.SceneManagement.SceneManager.LoadScene(n ? scene1 : scene2);
-            n = !n;
-        }
-
     }
 }
