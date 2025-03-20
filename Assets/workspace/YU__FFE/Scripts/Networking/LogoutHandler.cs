b6d3b6ad075550dbe2d05d2d8ade9f0b6788c71a
YU__FFE <yu0.0ffe@gmail.com>
2025-03-20 15:13:48 +0900

    refactor: 필요없는 주석 제거

--- Assets/workspace/YU__FFE/Scripts/Networking/LogoutHandler.cs
+++ Assets/workspace/YU__FFE/Scripts/Networking/LogoutHandler.cs
@@ -2,11 +2,8 @@ namespace workspace.YU__FFE.Scripts.Networking {
 
     public class LogoutManager : Singleton<LogoutManager> {
 
-        // ======================================================
-        //                      로그아웃
-        // ======================================================
-
         public void SignOut() {
+            
             TokenManager.Instance.ClearTokens();
             UI_Manager.Instance.Show(UI_Manager.PanelType.Login);
         }
