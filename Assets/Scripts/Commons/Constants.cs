961ac31dee902a35586162f8d3052fb50ffe0414
ham6 <hammin96@gmail.com>
2025-03-25 10:33:29 +0900

    refactor : GameResult를 constant로 변경

--- Assets/Scripts/Commons/Constants.cs
+++ Assets/Scripts/Commons/Constants.cs
@@ -15,6 +15,16 @@ namespace Commons {
             MultiPlayer,
         }
         
+        //게임 결과 (승패 판정)
+        public enum GameResult
+        {
+            None, // 게임 진행 중
+            Win, // 플레이어 승
+            Lose, // 플레이어 패
+            Draw // 비김
+        }
+        
+        
         public const string ServerURL = "http://localhost"; // Express 서버 URL
     }
 }
