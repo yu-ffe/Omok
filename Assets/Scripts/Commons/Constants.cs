4703ec6e0f400381eeef3fc3157054f9b81e00d6
Minji-GWON <classicming33@gmail.com>
2025-03-27 18:56:49 +0900

    듀얼플레이 결과 값 출력 보수
    playerA -> PlayerA승리 출력
    playerB -> PlayerB승리 출력

--- Assets/Scripts/Commons/Constants.cs
+++ Assets/Scripts/Commons/Constants.cs
@@ -37,5 +37,35 @@ namespace Commons {
 
         
         public const string ServerURL = "http://localhost"; // Express 서버 URL
+        
+        /// <summary>
+        /// PlayerType → GameResult 변환 메서드
+        /// </summary>
+        /// <param name="player"></param>
+        /// <returns></returns>
+        public static GameResult GetWinResultFromPlayerType(PlayerType player)
+        {
+            return player switch
+            {
+                PlayerType.PlayerA => GameResult.Player1Win,
+                PlayerType.PlayerB => GameResult.Player2Win,
+                _ => GameResult.None
+            };
+        }
+    
+        /// <summary>
+        /// GameResult → PlayerType 변환 메서드
+        /// </summary>
+        /// <param name="result"></param>
+        /// <returns></returns>
+        public static PlayerType GetWinnerPlayerFromGameResult(GameResult result)
+        {
+            return result switch
+            {
+                GameResult.Player1Win => PlayerType.PlayerA,
+                GameResult.Player2Win => PlayerType.PlayerB,
+                _ => PlayerType.None
+            };
+        }
     }
 }
