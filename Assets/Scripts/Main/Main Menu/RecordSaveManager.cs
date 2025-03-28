fd7714df2a94cf0e9b14972bc36300d2268b0831
KimHyeun <tenshi1207@gmail.com>
2025-03-28 11:03:24 +0900

    fix 기보 인덱스 밀림 버그 수정

--- Assets/Scripts/Main/Main Menu/RecordSaveManager.cs
+++ Assets/Scripts/Main/Main Menu/RecordSaveManager.cs
@@ -13,7 +13,7 @@ public class RecordSaveManager : Singleton<RecordSaveManager>
     public void SetLoadRecord(RecordData recordData) // 재생 기보 선택
     {
         loadedRecord = recordData;
-        loadIndex = 0;
+        loadIndex = -1;
 
 
         if (loadedRecord != null)
@@ -39,13 +39,11 @@ public class RecordSaveManager : Singleton<RecordSaveManager>
 
     public (Constants.PlayerType player, int x, int y) GetBeforeLocation() // 현재 기보 이전 수 좌표
     {
-        if (loadIndex > 0) // 최소 0번
+        if (loadIndex > 0)
         {
             loadIndex--;
-
             return (loadedRecord.Moves[loadIndex].player, loadedRecord.Moves[loadIndex].x, loadedRecord.Moves[loadIndex].y);
         }
-
         else
         {
             return (Constants.PlayerType.None, 0, 0); // 되돌아 갈 값 없음
@@ -54,29 +52,18 @@ public class RecordSaveManager : Singleton<RecordSaveManager>
 
     public (Constants.PlayerType player, int x, int y) GetAfterLocation() // 현재 기보 다음 수 좌표
     {
-        if (loadIndex < loadedRecord.Moves.Count - 1) // 최대 리스트 수 이하
+        if (loadIndex < loadedRecord.Moves.Count - 1) // 다음 수가 존재할 때만 증가
         {
             loadIndex++;
-
             return (loadedRecord.Moves[loadIndex].player, loadedRecord.Moves[loadIndex].x, loadedRecord.Moves[loadIndex].y);
         }
-
         else
         {
-            return (Constants.PlayerType.None, 0, 0); // 끝낰
+            return (Constants.PlayerType.None, 0, 0); // 끝
         }
     }
 
     #region 기보 착수 기능
-
-    /*
-     RecordSaveManager기보 착수 기능 레기온 다 만들어둠 필요하신곳에 이동시켜서 TODO표 표시해놓은 착수기능 추가해주시고
-ButtonSet 보시면 각 버튼들에 아까보신 RecordSaveManager 쪽 함수들로 연결되어있는데
-RecordSaveManager 인스턴스 대신 이동시키신곳 인스턴스로 변경해주시면 
-
-    추가 문제
-흰돌 반투명 보임, 클릭시 초록표시 뜸
-    */
 
     public void TurnGo(Func<(Constants.PlayerType, int, int)> getLocationFunc, bool isContinuous)
     {
