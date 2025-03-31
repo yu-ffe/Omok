4bb07ba857cb73e7552efbad262421f660781fb8
YU__FFE <yu0.0ffe@gmail.com>
2025-03-31 13:17:10 +0900

    [feat] 이미지 적용

--- Assets/Scripts/View/UI/Handler/DataUpdateHandler.cs
+++ Assets/Scripts/View/UI/Handler/DataUpdateHandler.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DataUpdateHandler : MonoBehaviour
 {
@@ -9,16 +10,17 @@ public class DataUpdateHandler : MonoBehaviour
     
     public TMP_Text txtCoin;
     public TMP_Text grade_name;
+    public Image image;
     
     // Update is called once per frame
     void Update()
     {
-        
-        if (Time.time >= nextUpdateTime)
-        {
+
+        if (Time.time >= nextUpdateTime) {
             txtCoin.text = PlayerManager.Instance.playerData.coins.ToString() + " 코인";
             grade_name.text = PlayerManager.Instance.playerData.grade.ToString() + "급\n" + PlayerManager.Instance.playerData.nickname;
-            // 이렇게 짜면 욕먹을테지만 상관없다.
+            image.sprite = PlayerManager.Instance.GetProfileSprites(PlayerManager.Instance.playerData.profileNum);
+        // 이렇게 짜면 욕먹을테지만 상관없다.
             // 눈속임만 완벽하면 해결될 일..
             nextUpdateTime = Time.time + 0.1f; // 1초 후 다시 실행
         }
